17070c39d0c9dcf38b8f27ffaa43a4a811225edd
Milton <3018849780101@ingenieria.usac.edu.gt>
2021-06-03 11:08:12 -0600

    Atributos xml
    utilizar estructura para atributos de nodos xml

--- src/clases/nodo_xml.ts
+++ src/clases/nodo_xml.ts
@@ -1,6 +1,8 @@
+import atr_xml from "./atr_xml"
+
 export default class nodo_xml{
     public id: string
-    public atributos: Array<string>
+    public atributos: Array<atr_xml>
     public valor: string
     public hijos: Array<nodo_xml>
     constructor(id,atributos,valor,hijos){
